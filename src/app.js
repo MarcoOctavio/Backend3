0f255e503caa3ab56e57ee53bbfa3db7605f171b
marcovenegas <marco.venegas.ch@gmail.com>
2026-04-11 02:00:51 -0600

    refactor: remove unused mocking router and related imports

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
-import mockingRouter from './routes/mocking.router.js';
 import mocksRouter from './routes/mocks.router.js';
 import dotenv from 'dotenv';
 import { errorHandler } from './middlewares/errorHandler.js';
--- src/routes/mocking.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMockingPets } from '../controllers/mocking.controllers.js';
-
-const router = Router();
-
-router.get('/mockingpets', getMockingPets);
-
-export default router;
